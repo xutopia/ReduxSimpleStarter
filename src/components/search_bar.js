8fa645b34bf550ac056d7b7026c733eabe5b6705
xutopia <xuology@gmail.com>
2016-12-27 13:48:09 -0500

    complete everything in Redux up to lecture 2

---
+++ src/components/search_bar.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+// const SearchBar = () => (<input />);
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange (term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
